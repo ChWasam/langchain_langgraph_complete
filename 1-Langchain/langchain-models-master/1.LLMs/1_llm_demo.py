66bb4c3facaf766f76c528d3c981e212b2909b5c
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2025-08-28 01:09:55 -0400

    updated

--- 1-Langchain/langchain-models-master/1.LLMs/1_llm_demo.py
+++ 1-Langchain/langchain-models-master/1.LLMs/1_llm_demo.py
@@ -1,3 +1,7 @@
+#  LLMs are not relevant anymore 
+# These days we use chatmodels 
+
+
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
 
@@ -8,3 +12,7 @@ llm = OpenAI(model='gpt-3.5-turbo-instruct')
 result = llm.invoke("What is the capital of India")
 
 print(result)
+
+
+
+#  Take input as string and give input as string 
--- 1-Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
+++ 1-Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
@@ -1,5 +1,4 @@
-#  LLMs are not relevant anymore 
-# These days we use chatmodels 
+
 
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
@@ -11,5 +10,3 @@ model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 result = model.invoke("Write a 5 line poem on cricket")
 
 print(result.content)
-
-#  Take input as string and give input as string 
