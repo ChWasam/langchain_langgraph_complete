e3c118467b82dd1307fcbd2d2c734c5580684a86
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2025-08-28 11:21:57 -0400

    Updated chatmodel openai

--- 1-Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
+++ 1-Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
@@ -10,3 +10,6 @@ model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 result = model.invoke("Write a 5 line poem on cricket")
 
 print(result.content)
+
+
+#  By max_completion_tokens you can restrict the no of tokens in output 
